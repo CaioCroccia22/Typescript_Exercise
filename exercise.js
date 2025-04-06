37d7096a1bdf66e584fcd0583cee63182fa5917d
caiocroccia22 <caiocroccia22@gmail.com>
2025-04-05 22:33:04 -0300

    feat: implement functions multiplies and sayHello

---
+++ exercise.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplies(valor1, valor2) {
+    return `O resultado é: ${valor1 * valor2}`;
+}
+function sayHello(name) {
+    return `Olá ${name}, como você está??`;
+}
+console.log(multiplies(10, 20));
+console.log(sayHello('Caio'));
---
+++ exercise.ts
@@ -0,0 +1,14 @@
+function multiplies(valor1: number, valor2: number): string{
+    return `O resultado é: ${valor1 * valor2}`
+}
+
+function sayHello(name: string){
+    return `Olá ${name}, como você está??`
+}
+
+
+
+console.log(multiplies(10, 20));
+
+
+console.log(sayHello('Caio'));
